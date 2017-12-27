6831b92295a0c3515ffead402dabb1caa63db19b
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-12-27 15:14:59 +0530

    Use repl or filereader depending on arguments provided

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,22 @@ mod evaluator;
 mod repl;
 mod filereader;
 
+use std::env;
+
 pub fn main() {
-    let file_name = "00-sample.bc";
-    let streamer = filereader::BCalcFileStreamer::new(file_name).unwrap();
-    let mut env = types::Environment::new();
-    for expr in streamer {
-        println!("{}\n", types::display(evaluator::evaluate(&mut env, expr)));
+    if env::args().len() == 1 {
+        println!(
+            "No arguments provided. Starting the REPL...\n Use Ctrl+C to quit.",
+        );
+        repl::init();
+    } else {
+        //Assuming only one argument provided for now
+        let file_name = env::args().into_iter().nth(1).unwrap();
+        println!("Parsing file {} and outputting the results", file_name);
+        let streamer = filereader::BCalcFileStreamer::new(&file_name).unwrap();
+        let mut env = types::Environment::new();
+        for expr in streamer {
+            println!("{}\n", types::display(evaluator::evaluate(&mut env, expr)));
+        }
     }
 }
